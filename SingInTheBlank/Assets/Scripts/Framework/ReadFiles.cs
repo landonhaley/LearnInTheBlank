2086c340729f0f3b9eb878dfa559a587e39cbc9c
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2016-07-20 19:56:08 -0400

    Created ReadFiles Script and added it to Welcome screen

---
+++ SingInTheBlank/Assets/Scripts/Framework/ReadFiles.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class ReadFiles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		populateGroupList ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void populateGroupList()
+	{
+		string filepath = "groups.txt", line;
+		FileStream in_file = new FileStream(filepath, FileMode.Open);
+
+		using (StreamReader readFile = new StreamReader(in_file))
+		{
+			while((line = readFile.ReadLine()) != null)
+			{
+				Group _group = new Group ();
+				_group.groupname = line;
+				ControlCenter.Instance.groups.Add (_group);
+			}	
+		}
+	}
+}
